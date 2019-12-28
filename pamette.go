c685a7c90584e0f74a3fb719f401d32b9cd872a1
Sylvain Laurent <s@6120.eu>
2019-12-28 17:19:05 +0100

    First draft of pam-eth registry contract

--- pamette.go
+++ pamette.go
@@ -28,16 +28,20 @@ var (
 )
 
 // PametteABI is the input ABI used to generate the binding from.
-const PametteABI = "[{\"constant\":true,\"inputs\":[],\"name\":\"generateOTP\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"internalType\":\"bytes\",\"name\":\"data\",\"type\":\"bytes\"},{\"internalType\":\"bytes\",\"name\":\"signature\",\"type\":\"bytes\"}],\"name\":\"isAuthorized\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"
+const PametteABI = "[{\"inputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"}],\"name\":\"AllowedUser\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"userName\",\"type\":\"string\"},{\"internalType\":\"uint256\",\"name\":\"userId\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"machineId\",\"type\":\"uint256\"},{\"internalType\":\"bytes32\",\"name\":\"sharedPassword\",\"type\":\"bytes32\"}],\"name\":\"generateOTP\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"},{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"userName\",\"type\":\"string\"},{\"internalType\":\"uint256\",\"name\":\"userId\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"machineId\",\"type\":\"uint256\"},{\"internalType\":\"bytes32\",\"name\":\"sharedPassword\",\"type\":\"bytes32\"}],\"name\":\"generateUserHash\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"}],\"stateMutability\":\"pure\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"userName\",\"type\":\"string\"},{\"internalType\":\"uint256\",\"name\":\"userId\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"machineId\",\"type\":\"uint256\"},{\"internalType\":\"bytes32\",\"name\":\"sharedPassword\",\"type\":\"bytes32\"},{\"internalType\":\"uint256\",\"name\":\"blockNumber\",\"type\":\"uint256\"},{\"internalType\":\"uint8\",\"name\":\"v\",\"type\":\"uint8\"},{\"internalType\":\"bytes32\",\"name\":\"r\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"s\",\"type\":\"bytes32\"}],\"name\":\"isAuthorized\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"owner\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"hash\",\"type\":\"bytes32\"},{\"internalType\":\"address\",\"name\":\"user\",\"type\":\"address\"}],\"name\":\"setUser\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
 
 // PametteFuncSigs maps the 4-byte function signature to its string representation.
 var PametteFuncSigs = map[string]string{
-	"08e465d0": "generateOTP()",
-	"f54aca07": "isAuthorized(bytes,bytes)",
+	"a46dfcd0": "AllowedUser(bytes32)",
+	"a247e33b": "generateOTP(string,uint256,uint256,bytes32)",
+	"2bd5c8d9": "generateUserHash(string,uint256,uint256,bytes32)",
+	"210d879e": "isAuthorized(string,uint256,uint256,bytes32,uint256,uint8,bytes32,bytes32)",
+	"8da5cb5b": "owner()",
+	"9476b3e2": "setUser(bytes32,address)",
 }
 
 // PametteBin is the compiled bytecode used for deploying new contracts.
-var PametteBin = "0x608060405234801561001057600080fd5b5061027e806100206000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c806308e465d01461003b578063f54aca07146100b8575b600080fd5b6100436101f9565b6040805160208082528351818301528351919283929083019185019080838360005b8381101561007d578181015183820152602001610065565b50505050905090810190601f1680156100aa5780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b6101e5600480360360408110156100ce57600080fd5b8101906020810181356401000000008111156100e957600080fd5b8201836020820111156100fb57600080fd5b8035906020019184600183028401116401000000008311171561011d57600080fd5b91908080601f016020809104026020016040519081016040528093929190818152602001838380828437600092019190915250929594936020810193503591505064010000000081111561017057600080fd5b82018360208201111561018257600080fd5b803590602001918460018302840111640100000000831117156101a457600080fd5b91908080601f01602080910402602001604051908101604052809392919081815260200183838082843760009201919091525092955061021b945050505050565b604080519115158252519081900360200190f35b6040805180820190915260088152671c185cdcdddbdc9960c21b602082015290565b60019291505056fea265627a7a723158208f08c8210f9dc774d17287dcbfe884a7763423416109a1ec27239976411d2ce364736f6c637828302e352e31342d646576656c6f702e323031392e31322e372b636f6d6d69742e31666531343539620058"
+var PametteBin = "0x608060405234801561001057600080fd5b50600080546001600160a01b031916331790556107c2806100326000396000f3fe608060405234801561001057600080fd5b50600436106100625760003560e01c8063210d879e146100675780632bd5c8d9146101485780638da5cb5b1461020b5780639476b3e21461022f578063a247e33b1461025d578063a46dfcd014610327575b600080fd5b610134600480360361010081101561007e57600080fd5b81019060208101813564010000000081111561009957600080fd5b8201836020820111156100ab57600080fd5b803590602001918460018302840111640100000000831117156100cd57600080fd5b91908080601f0160208091040260200160405190810160405280939291908181526020018383808284376000920191909152509295505082359350505060208101359060408101359060608101359060ff6080820135169060a08101359060c00135610344565b604080519115158252519081900360200190f35b6101f96004803603608081101561015e57600080fd5b81019060208101813564010000000081111561017957600080fd5b82018360208201111561018b57600080fd5b803590602001918460018302840111640100000000831117156101ad57600080fd5b91908080601f01602080910402602001604051908101604052809392919081815260200183838082843760009201919091525092955050823593505050602081013590604001356105a1565b60408051918252519081900360200190f35b61021361064e565b604080516001600160a01b039092168252519081900360200190f35b61025b6004803603604081101561024557600080fd5b50803590602001356001600160a01b031661065d565b005b61030e6004803603608081101561027357600080fd5b81019060208101813564010000000081111561028e57600080fd5b8201836020820111156102a057600080fd5b803590602001918460018302840111640100000000831117156102c257600080fd5b91908080601f0160208091040260200160405190810160405280939291908181526020018383808284376000920191909152509295505082359350505060208101359060400135610726565b6040805192835260208301919091528051918290030190f35b6102136004803603602081101561033d57600080fd5b5035610771565b600060058543031115610392576040805162461bcd60e51b815260206004820152601160248201527012185cda081d1a5b59481a5b9d985b1a59607a1b604482015290519081900360640190fd5b60006103a08a8a8a8a6105a1565b6000818152600160205260409020549091506001600160a01b03166103fe576040805162461bcd60e51b815260206004820152600f60248201526e155cd95c881a5b995e1a5cdd185b9d608a1b604482015290519081900360640190fd5b60408051602080820184905243828401528251808303840181526060830180855281519183019190912060a084018552601c8083527f19457468657265756d205369676e6564204d6573736167653a0a3133000000006080909501948552945190949193600093859387939091019182918083835b602083106104925780518252601f199092019160209182019101610473565b51815160209384036101000a6000190180199092169116179052920193845250604080518085038152848301808352815191840191909120600090915281850180835281905260ff8e166060860152608085018d905260a085018c905290519095506001945060c080850194929350601f198201928290030190855afa158015610520573d6000803e3d6000fd5b505060408051601f1901516000878152600160205291909120546001600160a01b03908116911614905061058e576040805162461bcd60e51b815260206004820152601060248201526f155cd95c881d5b9c195c9b5a5d1d195960821b604482015290519081900360640190fd5b5060019c9b505050505050505050505050565b6000848484846040516020018080602001858152602001848152602001838152602001828103825286818151815260200191508051906020019080838360005b838110156105f95781810151838201526020016105e1565b50505050905090810190601f1680156106265780820380516001836020036101000a031916815260200191505b5095505050505050604051602081830303815290604052805190602001209050949350505050565b6000546001600160a01b031681565b6000546001600160a01b031633146106bc576040805162461bcd60e51b815260206004820181905260248201527f4f6e6c79206f776e65722063616e2075736520746869732066756e6374696f6e604482015290519081900360640190fd5b6000828152600160205260409020546001600160a01b0316156106fa57600082815260016020526040902080546001600160a01b0319169055610722565b600082815260016020526040902080546001600160a01b0319166001600160a01b0383161790555b5050565b6000806000610737878787876105a1565b6040805160208082019390935243818301819052825180830384018152606090920190925280519201919091209890975095505050505050565b6001602052600090815260409020546001600160a01b03168156fea2646970667358221220fbe1ed87040886b93d60e3a0bdfd77520566661b61ad065e060f634141d8bcc664736f6c63430006000033"
 
 // DeployPamette deploys a new Ethereum contract, binding an instance of Pamette to it.
 func DeployPamette(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Pamette, error) {
@@ -195,54 +199,158 @@ func (_Pamette *PametteTransactorRaw) Transact(opts *bind.TransactOpts, method s
 	return _Pamette.Contract.contract.Transact(opts, method, params...)
 }
 
-// GenerateOTP is a free data retrieval call binding the contract method 0x08e465d0.
+// AllowedUser is a free data retrieval call binding the contract method 0xa46dfcd0.
 //
-// Solidity: function generateOTP() constant returns(string)
-func (_Pamette *PametteCaller) GenerateOTP(opts *bind.CallOpts) (string, error) {
+// Solidity: function AllowedUser(bytes32 ) constant returns(address)
+func (_Pamette *PametteCaller) AllowedUser(opts *bind.CallOpts, arg0 [32]byte) (common.Address, error) {
 	var (
-		ret0 = new(string)
+		ret0 = new(common.Address)
 	)
 	out := ret0
-	err := _Pamette.contract.Call(opts, out, "generateOTP")
+	err := _Pamette.contract.Call(opts, out, "AllowedUser", arg0)
 	return *ret0, err
 }
 
-// GenerateOTP is a free data retrieval call binding the contract method 0x08e465d0.
+// AllowedUser is a free data retrieval call binding the contract method 0xa46dfcd0.
 //
-// Solidity: function generateOTP() constant returns(string)
-func (_Pamette *PametteSession) GenerateOTP() (string, error) {
-	return _Pamette.Contract.GenerateOTP(&_Pamette.CallOpts)
+// Solidity: function AllowedUser(bytes32 ) constant returns(address)
+func (_Pamette *PametteSession) AllowedUser(arg0 [32]byte) (common.Address, error) {
+	return _Pamette.Contract.AllowedUser(&_Pamette.CallOpts, arg0)
 }
 
-// GenerateOTP is a free data retrieval call binding the contract method 0x08e465d0.
+// AllowedUser is a free data retrieval call binding the contract method 0xa46dfcd0.
 //
-// Solidity: function generateOTP() constant returns(string)
-func (_Pamette *PametteCallerSession) GenerateOTP() (string, error) {
-	return _Pamette.Contract.GenerateOTP(&_Pamette.CallOpts)
+// Solidity: function AllowedUser(bytes32 ) constant returns(address)
+func (_Pamette *PametteCallerSession) AllowedUser(arg0 [32]byte) (common.Address, error) {
+	return _Pamette.Contract.AllowedUser(&_Pamette.CallOpts, arg0)
 }
 
-// IsAuthorized is a free data retrieval call binding the contract method 0xf54aca07.
+// GenerateOTP is a free data retrieval call binding the contract method 0xa247e33b.
 //
-// Solidity: function isAuthorized(bytes data, bytes signature) constant returns(bool)
-func (_Pamette *PametteCaller) IsAuthorized(opts *bind.CallOpts, data []byte, signature []byte) (bool, error) {
+// Solidity: function generateOTP(string userName, uint256 userId, uint256 machineId, bytes32 sharedPassword) constant returns(bytes32, uint256)
+func (_Pamette *PametteCaller) GenerateOTP(opts *bind.CallOpts, userName string, userId *big.Int, machineId *big.Int, sharedPassword [32]byte) ([32]byte, *big.Int, error) {
+	var (
+		ret0 = new([32]byte)
+		ret1 = new(*big.Int)
+	)
+	out := &[]interface{}{
+		ret0,
+		ret1,
+	}
+	err := _Pamette.contract.Call(opts, out, "generateOTP", userName, userId, machineId, sharedPassword)
+	return *ret0, *ret1, err
+}
+
+// GenerateOTP is a free data retrieval call binding the contract method 0xa247e33b.
+//
+// Solidity: function generateOTP(string userName, uint256 userId, uint256 machineId, bytes32 sharedPassword) constant returns(bytes32, uint256)
+func (_Pamette *PametteSession) GenerateOTP(userName string, userId *big.Int, machineId *big.Int, sharedPassword [32]byte) ([32]byte, *big.Int, error) {
+	return _Pamette.Contract.GenerateOTP(&_Pamette.CallOpts, userName, userId, machineId, sharedPassword)
+}
+
+// GenerateOTP is a free data retrieval call binding the contract method 0xa247e33b.
+//
+// Solidity: function generateOTP(string userName, uint256 userId, uint256 machineId, bytes32 sharedPassword) constant returns(bytes32, uint256)
+func (_Pamette *PametteCallerSession) GenerateOTP(userName string, userId *big.Int, machineId *big.Int, sharedPassword [32]byte) ([32]byte, *big.Int, error) {
+	return _Pamette.Contract.GenerateOTP(&_Pamette.CallOpts, userName, userId, machineId, sharedPassword)
+}
+
+// GenerateUserHash is a free data retrieval call binding the contract method 0x2bd5c8d9.
+//
+// Solidity: function generateUserHash(string userName, uint256 userId, uint256 machineId, bytes32 sharedPassword) constant returns(bytes32)
+func (_Pamette *PametteCaller) GenerateUserHash(opts *bind.CallOpts, userName string, userId *big.Int, machineId *big.Int, sharedPassword [32]byte) ([32]byte, error) {
+	var (
+		ret0 = new([32]byte)
+	)
+	out := ret0
+	err := _Pamette.contract.Call(opts, out, "generateUserHash", userName, userId, machineId, sharedPassword)
+	return *ret0, err
+}
+
+// GenerateUserHash is a free data retrieval call binding the contract method 0x2bd5c8d9.
+//
+// Solidity: function generateUserHash(string userName, uint256 userId, uint256 machineId, bytes32 sharedPassword) constant returns(bytes32)
+func (_Pamette *PametteSession) GenerateUserHash(userName string, userId *big.Int, machineId *big.Int, sharedPassword [32]byte) ([32]byte, error) {
+	return _Pamette.Contract.GenerateUserHash(&_Pamette.CallOpts, userName, userId, machineId, sharedPassword)
+}
+
+// GenerateUserHash is a free data retrieval call binding the contract method 0x2bd5c8d9.
+//
+// Solidity: function generateUserHash(string userName, uint256 userId, uint256 machineId, bytes32 sharedPassword) constant returns(bytes32)
+func (_Pamette *PametteCallerSession) GenerateUserHash(userName string, userId *big.Int, machineId *big.Int, sharedPassword [32]byte) ([32]byte, error) {
+	return _Pamette.Contract.GenerateUserHash(&_Pamette.CallOpts, userName, userId, machineId, sharedPassword)
+}
+
+// IsAuthorized is a free data retrieval call binding the contract method 0x210d879e.
+//
+// Solidity: function isAuthorized(string userName, uint256 userId, uint256 machineId, bytes32 sharedPassword, uint256 blockNumber, uint8 v, bytes32 r, bytes32 s) constant returns(bool)
+func (_Pamette *PametteCaller) IsAuthorized(opts *bind.CallOpts, userName string, userId *big.Int, machineId *big.Int, sharedPassword [32]byte, blockNumber *big.Int, v uint8, r [32]byte, s [32]byte) (bool, error) {
 	var (
 		ret0 = new(bool)
 	)
 	out := ret0
-	err := _Pamette.contract.Call(opts, out, "isAuthorized", data, signature)
+	err := _Pamette.contract.Call(opts, out, "isAuthorized", userName, userId, machineId, sharedPassword, blockNumber, v, r, s)
 	return *ret0, err
 }
 
-// IsAuthorized is a free data retrieval call binding the contract method 0xf54aca07.
+// IsAuthorized is a free data retrieval call binding the contract method 0x210d879e.
 //
-// Solidity: function isAuthorized(bytes data, bytes signature) constant returns(bool)
-func (_Pamette *PametteSession) IsAuthorized(data []byte, signature []byte) (bool, error) {
-	return _Pamette.Contract.IsAuthorized(&_Pamette.CallOpts, data, signature)
+// Solidity: function isAuthorized(string userName, uint256 userId, uint256 machineId, bytes32 sharedPassword, uint256 blockNumber, uint8 v, bytes32 r, bytes32 s) constant returns(bool)
+func (_Pamette *PametteSession) IsAuthorized(userName string, userId *big.Int, machineId *big.Int, sharedPassword [32]byte, blockNumber *big.Int, v uint8, r [32]byte, s [32]byte) (bool, error) {
+	return _Pamette.Contract.IsAuthorized(&_Pamette.CallOpts, userName, userId, machineId, sharedPassword, blockNumber, v, r, s)
 }
 
-// IsAuthorized is a free data retrieval call binding the contract method 0xf54aca07.
+// IsAuthorized is a free data retrieval call binding the contract method 0x210d879e.
 //
-// Solidity: function isAuthorized(bytes data, bytes signature) constant returns(bool)
-func (_Pamette *PametteCallerSession) IsAuthorized(data []byte, signature []byte) (bool, error) {
-	return _Pamette.Contract.IsAuthorized(&_Pamette.CallOpts, data, signature)
+// Solidity: function isAuthorized(string userName, uint256 userId, uint256 machineId, bytes32 sharedPassword, uint256 blockNumber, uint8 v, bytes32 r, bytes32 s) constant returns(bool)
+func (_Pamette *PametteCallerSession) IsAuthorized(userName string, userId *big.Int, machineId *big.Int, sharedPassword [32]byte, blockNumber *big.Int, v uint8, r [32]byte, s [32]byte) (bool, error) {
+	return _Pamette.Contract.IsAuthorized(&_Pamette.CallOpts, userName, userId, machineId, sharedPassword, blockNumber, v, r, s)
 }
+
+// Owner is a free data retrieval call binding the contract method 0x8da5cb5b.
+//
+// Solidity: function owner() constant returns(address)
+func (_Pamette *PametteCaller) Owner(opts *bind.CallOpts) (common.Address, error) {
+	var (
+		ret0 = new(common.Address)
+	)
+	out := ret0
+	err := _Pamette.contract.Call(opts, out, "owner")
+	return *ret0, err
+}
+
+// Owner is a free data retrieval call binding the contract method 0x8da5cb5b.
+//
+// Solidity: function owner() constant returns(address)
+func (_Pamette *PametteSession) Owner() (common.Address, error) {
+	return _Pamette.Contract.Owner(&_Pamette.CallOpts)
+}
+
+// Owner is a free data retrieval call binding the contract method 0x8da5cb5b.
+//
+// Solidity: function owner() constant returns(address)
+func (_Pamette *PametteCallerSession) Owner() (common.Address, error) {
+	return _Pamette.Contract.Owner(&_Pamette.CallOpts)
+}
+
+// SetUser is a paid mutator transaction binding the contract method 0x9476b3e2.
+//
+// Solidity: function setUser(bytes32 hash, address user) returns()
+func (_Pamette *PametteTransactor) SetUser(opts *bind.TransactOpts, hash [32]byte, user common.Address) (*types.Transaction, error) {
+	return _Pamette.contract.Transact(opts, "setUser", hash, user)
+}
+
+// SetUser is a paid mutator transaction binding the contract method 0x9476b3e2.
+//
+// Solidity: function setUser(bytes32 hash, address user) returns()
+func (_Pamette *PametteSession) SetUser(hash [32]byte, user common.Address) (*types.Transaction, error) {
+	return _Pamette.Contract.SetUser(&_Pamette.TransactOpts, hash, user)
+}
+
+// SetUser is a paid mutator transaction binding the contract method 0x9476b3e2.
+//
+// Solidity: function setUser(bytes32 hash, address user) returns()
+func (_Pamette *PametteTransactorSession) SetUser(hash [32]byte, user common.Address) (*types.Transaction, error) {
+	return _Pamette.Contract.SetUser(&_Pamette.TransactOpts, hash, user)
+}
+
--- pamette.sol
+++ pamette.sol
@@ -3,30 +3,50 @@ pragma solidity >=0.4.22 <0.7.0;
 contract Pamette {
 	address public owner;
 
-	mapping(uint256 => bool) public AllowUser
-	constructor() {
+	mapping(bytes32 => address) public AllowedUser;
+
+	constructor() public {
 		owner = msg.sender;
 	}
 
-	
-	function setUser(uint256 hash) void public {
+	function setUser(bytes32 hash, address user) public {
 		require(msg.sender == owner, "Only owner can use this function");
-		if (AllowedUser[hash]) {
+		// TODO handle list
+		if (AllowedUser[hash] != address(0)) {
 			delete AllowedUser[hash];
 		} else {
-			AllowedUser[hash] = true;
+			AllowedUser[hash] = user;
 		}
 	}
-	
-	function isAuthorized(bytes memory data, bytes memory signature) public view
+
+	function isAuthorized(string memory userName, uint256 userId, uint256 machineId, bytes32 sharedPassword, uint256 blockNumber, uint8 v, bytes32 r, bytes32 s) public view
 		returns (bool)
 	{
+		require(block.number - blockNumber <= 5, "Hash time invalid");
+		bytes32 userHash = generateUserHash(userName, userId, machineId, sharedPassword);
+		require(AllowedUser[userHash] != address(0), "User inexistant");
+		bytes32 hashToSign = keccak256(abi.encode(userHash, block.number));
+
+		bytes memory prefix = "\x19Ethereum Signed Message:\n13";
+		bytes32 prefixedHash = keccak256(abi.encodePacked(prefix, hashToSign));
+		require(AllowedUser[userHash] == ecrecover(prefixedHash, v, r, s), "User unpermitted");
+
 		return true;
 	}
-	
-	function generateOTP() public view
-		returns (string memory)
+
+	function generateUserHash(string memory userName, uint256 userId, uint256 machineId, bytes32 sharedPassword) public pure
+	    returns (bytes32)
 	{
-		return "password";
+		return keccak256(abi.encode(userName, userId, machineId, sharedPassword));
+	}
+
+	function generateOTP(string memory userName, uint256 userId, uint256 machineId, bytes32 sharedPassword) public view
+		returns (bytes32 , uint)
+	{
+		bytes32 userHash = generateUserHash(userName, userId, machineId, sharedPassword);
+		// Contatenate user + machineId + sharedPassword + currentBlockNumber
+		bytes32 hashToSign = keccak256(abi.encode(userHash, block.number));
+
+		return (hashToSign, block.number);
 	}
 }
